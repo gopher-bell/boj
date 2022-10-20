4c87eb3842c52683231375dbf55938a95f0ab7c5
gopher-bell <gopher.bell@gmail.com>
2022-10-20 23:56:26 +0900

    boj: 10809

--- 10809/10809.go
+++ 10809/10809.go
@@ -18,18 +18,22 @@ func main() {
 	scanner.Scan()
 	b := bytes.Fields(scanner.Bytes())
 	comp := make([]int, 26)
+
 	for i := range comp {
 		comp[i] = -1
 	}
+
 	res := make([]byte, len(b[0]))
 	for i := range b[0] {
 		res[i] = b[0][i]
 	}
+
 	for i := range res {
 		if comp[res[i]-97] == -1 {
 			comp[res[i]-97] = i
 		}
 	}
+
 	for i := range comp {
 		writer.WriteString(strconv.Itoa(comp[i]) + " ")
 	}
