42a92159e70a7849d0024231e00c699e9a8312c8
gopher-bell <gopher.bell@gmail.com>
2022-11-18 18:33:29 +0900

    이코테 삽입정렬

--- ecote/sort/insertion/insertionSort.go
+++ ecote/sort/insertion/insertionSort.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func insertionSort(nums []int) {
+	for i := range nums {
+		for j := i; j > 0; j-- {
+			if nums[j] < nums[j-1] {
+				fmt.Println(nums)
+				nums[j], nums[j-1] = nums[j-1], nums[j]
+			} else {
+				break
+			}
+		}
+	}
+}
+
+func main() {
+	s := []int{1, 6, 10, 9, 2, 7, 8, 3, 5, 4}
+	insertionSort(s)
+	fmt.Println(s)
+}
