7efc1512012f70c2476c3442e1a2c603d0bcf41b
gopher-bell <gopher.bell@gmail.com>
2022-12-03 16:17:49 +0900

    프로그래머스 명예의전당

--- programmers/명예의전당/명예의전당.go
+++ programmers/명예의전당/명예의전당.go
@@ -1,1 +1,20 @@
 package 명예의전당
+
+import "sort"
+
+func solution(k int, score []int) []int {
+	var res []int
+	var ans []int
+	for i := range score {
+		res = append(res, score[i])
+		sort.Slice(res, func(i, j int) bool {
+			return res[i] < res[j]
+		})
+		if len(res) > k {
+			res = res[1:]
+		}
+		ans = append(ans, res[0])
+	}
+
+	return ans
+}
