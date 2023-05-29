b5e461477191181eea33566c5310a6e036ae3366
Jong <gopher.bell@gmail.com>
2023-05-30 00:01:04 +0900

    boj: 11328

--- 11328/11328.go
+++ 11328/11328.go
@@ -27,17 +27,14 @@ func main() {
 		}
 
 		arr1 := make([]int, 26)
-		arr2 := make([]int, 26)
 		for j := range str1 {
 			arr1[str1[j]-'a']++
-		}
-		for k := range str2 {
-			arr2[str2[k]-'a']++
+			arr1[str2[j]-'a']--
 		}
 
 		flag := true
 		for l := 0; l < 26; l++ {
-			if arr1[l] != arr2[l] {
+			if arr1[l] != 0 {
 				flag = false
 				break
 			}
