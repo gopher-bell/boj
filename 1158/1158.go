4288f5ce22dac1f0513c91cba9aaef55bc1ed4b8
Jong <gopher.bell@gmail.com>
2023-06-03 15:18:13 +0900

    boj: 1158

--- 1158/1158.go
+++ 1158/1158.go
@@ -2,45 +2,39 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	scanner = bufio.NewScanner(os.Stdin)
+	writer  = bufio.NewWriter(os.Stdout)
 )
 
 func main() {
-	var n, m int
-	fmt.Scan(&n, &m)
-	print(solve(n, m))
-}
+	defer writer.Flush()
+	scanner.Scan()
+	s := strings.Fields(scanner.Text())
+	s1, _ := strconv.Atoi(s[0])
+	s2, _ := strconv.Atoi(s[1])
 
-func print(seq []int) {
-	w := bufio.NewWriter(os.Stdout)
-
-	fmt.Fprint(w, "<")
-
-	for i, n := range seq {
-		if i > 0 {
-			fmt.Fprint(w, ", ")
-		}
-		fmt.Fprint(w, n)
-	}
-
-	fmt.Fprintln(w, ">")
-	w.Flush()
-}
-
-func solve(n, m int) []int {
-	ring := make([]int, n)
-
-	for i := 0; i < n; i++ {
+	ring := make([]int, s1)
+	for i := 0; i < s1; i++ {
 		ring[i] = i + 1
 	}
 
-	removed := make([]int, n)
-	for i := 0; i < n; i++ {
-		j := (m - 1) % len(ring)
-		removed[i] = ring[j]
-		ring = append(ring[j+1:], ring[:j]...)
+	writer.WriteString("<")
+
+	j := 0
+	for i := 0; i < s1; i++ {
+		if i > 0 {
+			writer.WriteString(", ")
+		}
+		j = (j + s2 - 1) % len(ring)
+		writer.WriteString(strconv.Itoa(ring[j]))
+		ring = append(ring[:j], ring[j+1:]...)
 	}
 
-	return removed
+	writer.WriteString(">")
 }
