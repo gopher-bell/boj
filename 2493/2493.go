3904d6dcf1ca02a94db00e4cb47f24c1fd8aba16
Jong <gopher.bell@gmail.com>
2023-06-04 21:37:51 +0900

    boj: 2493

--- 2493/2493.go
+++ 2493/2493.go
@@ -7,39 +7,54 @@ import (
 )
 
 var (
-	scanner = bufio.NewScanner(os.Stdin)
 	writer  = bufio.NewWriter(os.Stdout)
+	scanner = bufio.NewScanner(os.Stdin)
 )
 
-type stack struct {
-	idx int
-	val int
+type tower struct {
+	idx    int
+	height int
+}
+
+func getNumber() int {
+	scanner.Scan()
+	b := scanner.Bytes()
+	ans := 0
+	tmp := 0
+
+	for _, v := range b {
+		ans *= 10
+		tmp = int(v - '0')
+		ans += tmp
+		tmp = 0
+	}
+
+	return ans
 }
 
 func main() {
 	defer writer.Flush()
 	scanner.Split(bufio.ScanWords)
-	scanner.Scan()
-	nums, _ := strconv.Atoi(scanner.Text())
-	s := make([]stack, 0, nums)
+	nums := getNumber()
+	stack := make([]tower, 0, nums)
 	for i := 0; i < nums; i++ {
-		scanner.Scan()
-		tower, _ := strconv.Atoi(scanner.Text())
+		height := getNumber()
 		flag := true
-		for len(s) > 0 {
-			if s[len(s)-1].val > tower {
-				writer.WriteString(strconv.Itoa(s[len(s)-1].idx) + " ")
-				s = append(s, stack{i + 1, tower})
+		for len(stack) > 0 {
+			last := len(stack) - 1
+			if stack[last].height >= height {
+				writer.WriteString(strconv.Itoa(stack[last].idx) + " ")
+				stack = append(stack, tower{idx: i + 1, height: height})
 				flag = false
 				break
 			} else {
-				s = s[:len(s)-1]
+				stack = stack[:last]
 			}
 		}
 
 		if flag {
+			stack = append(stack, tower{idx: i + 1, height: height})
 			writer.WriteString("0 ")
-			s = append(s, stack{i + 1, tower})
 		}
 	}
 }
