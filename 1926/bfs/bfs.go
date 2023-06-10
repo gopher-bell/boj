4e2b2827b7a8db60c2af271ecbdcd18364e34d24
Jong <gopher.bell@gmail.com>
2023-06-10 16:48:43 +0900

    boj: bfs

--- 1926/bfs/bfs.go
+++ 1926/bfs/bfs.go
@@ -4,87 +4,84 @@ import (
 	"bufio"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var (
-	scanner   = bufio.NewScanner(os.Stdin)
-	writer    = bufio.NewWriter(os.Stdout)
-	dx        = []int{1, 0, -1, 0}
-	dy        = []int{0, 1, 0, -1}
-	graph     [][]int
-	check     [][]bool
-	totalDraw int
-	MaxDraw   int
+	scanner = bufio.NewScanner(os.Stdin)
+	writer  = bufio.NewWriter(os.Stdout)
+	dx      = []int{1, 0, -1, 0}
+	dy      = []int{0, 1, 0, -1}
 )
 
-func readStdin() []int {
+type pair struct {
+	x int
+	y int
+}
+
+func getNumber() int {
 	scanner.Scan()
-	s := strings.Fields(scanner.Text())
-	res := make([]int, len(s))
-	for i := range s {
-		t, _ := strconv.Atoi(s[i])
-		res[i] = t
+	r := 0
+	for _, c := range scanner.Bytes() {
+		r *= 10
+		r += int(c - '0')
 	}
-	return res
+	return r
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-
-	return b
-}
-
-func bfs(y, x int) int {
-	var res = 1
-	queue := [][]int{{y, x}}
-	for len(queue) != 0 {
+func bfs(graph [][]int, visited [][]bool, x, y int) (int, bool) {
+	ret := false
+	cnt := 1
+	queue := []pair{{x, y}}
+	for len(queue) > 0 {
 		dq := queue[0]
 		queue = queue[1:]
-
 		for i := 0; i < 4; i++ {
-			ny := dq[0] + dy[i]
-			nx := dq[1] + dx[i]
-			if (0 <= ny && ny < len(graph)) && (0 <= nx && nx < len(graph[0])) {
-				if graph[ny][nx] == 1 && !check[ny][nx] {
-					check[ny][nx] = true
-					res++
-					queue = append(queue, []int{ny, nx})
+			nx := dx[i] + dq.x
+			ny := dy[i] + dq.y
+			if nx < len(graph) && nx >= 0 && ny < len(graph[0]) && ny >= 0 {
+				if !visited[nx][ny] && graph[nx][ny] == 1 {
+					visited[nx][ny] = true
+					queue = append(queue, pair{nx, ny})
+					cnt++
+				}
+			}
+		}
+		ret = true
+	}
+
+	return cnt, ret
+}
+
+func main() {
+	defer writer.Flush()
+	scanner.Split(bufio.ScanWords)
+	n1, n2 := getNumber(), getNumber()
+	graph := make([][]int, n1)
+	visited := make([][]bool, n1)
+	for i := range graph {
+		visited[i] = make([]bool, n2)
+		graph[i] = make([]int, n2)
+		for j := range graph[i] {
+			graph[i][j] = getNumber()
+		}
+	}
+
+	cnt := 0
+	max := 0
+	for i := range visited {
+		for j := range visited[i] {
+			if !visited[i][j] && graph[i][j] == 1 {
+				visited[i][j] = true
+				tot, success := bfs(graph, visited, i, j)
+				if success {
+					cnt++
+				}
+				if tot > max {
+					max = tot
 				}
 			}
 		}
 	}
 
-	return res
-}
-
-func main() {
-	defer writer.Flush()
-	scanner.Split(bufio.ScanLines)
-
-	n1 := readStdin()
-
-	graph = make([][]int, n1[0])
-	check = make([][]bool, n1[0])
-
-	for i := 0; i < n1[0]; i++ {
-		graph[i] = readStdin()
-		check[i] = make([]bool, n1[1])
-	}
-
-	for i := range graph {
-		for j := range graph[i] {
-			if graph[i][j] == 1 && !check[i][j] {
-				check[i][j] = true
-				totalDraw++
-				MaxDraw = max(MaxDraw, bfs(i, j))
-			}
-		}
-	}
-
-	writer.WriteString(strconv.Itoa(totalDraw))
-	writer.WriteString("\n")
-	writer.WriteString(strconv.Itoa(MaxDraw))
+	writer.WriteString(strconv.Itoa(cnt) + "\n" + strconv.Itoa(max))
 }
