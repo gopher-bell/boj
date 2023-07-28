3eceff6556e29154338b64b34c838e17101d0304
Jong <gopher.bell@gmail.com>
2023-07-28 18:39:34 +0900

    boj: 2178

--- 2178/2178.go
+++ 2178/2178.go
@@ -8,40 +8,42 @@ import (
 )
 
 var (
-	scanner = bufio.NewScanner(os.Stdin)
 	writer  = bufio.NewWriter(os.Stdout)
+	scanner = bufio.NewScanner(os.Stdin)
 	dx      = []int{1, 0, -1, 0}
 	dy      = []int{0, 1, 0, -1}
 )
 
-type queue struct {
+type q struct {
 	x int
 	y int
 }
 
-func getNumbers() []int {
-	scanner.Scan()
-	nums := make([]int, 0)
-	for _, v := range scanner.Bytes() {
-		nums = append(nums, int(v-'0'))
+func transferSlice(s []string, rg int) []int {
+	n := make([]int, rg)
+	for i := 0; i < rg; i++ {
+		n[i], _ = strconv.Atoi(s[i])
 	}
 
-	return nums
+	return n
 }
 
-func bfs(graph [][]int, x, y int) {
-	q := []queue{{x: x, y: y}}
-	for len(q) > 0 {
-		dq := q[0]
-		q = q[1:]
+func bfs(graph [][]int) {
+	queue := []q{{0, 0}}
+	for len(queue) > 0 {
+		dq := queue[0]
+		queue = queue[1:]
 		for i := 0; i < 4; i++ {
-			nx := dq.x + dx[i]
-			ny := dq.y + dy[i]
-			if nx < 0 || nx >= len(graph) || ny < 0 || ny >= len(graph[0]) || graph[nx][ny] != 1 {
+			nx, ny := dq.x+dx[i], dq.y+dy[i]
+			if nx < 0 || nx >= len(graph) || ny < 0 || ny >= len(graph[0]) {
 				continue
 			}
+			if graph[nx][ny] == 0 || graph[nx][ny] != 1 {
+				continue
+			}
+
 			graph[nx][ny] = graph[dq.x][dq.y] + 1
-			q = append(q, queue{x: nx, y: ny})
+			queue = append(queue, q{nx, ny})
 		}
 	}
 }
@@ -49,22 +51,18 @@ func bfs(graph [][]int, x, y int) {
 func main() {
 	defer writer.Flush()
 	scanner.Scan()
-	input := strings.Fields(scanner.Text())
-	n, _ := strconv.Atoi(input[0])
+	s := strings.Split(scanner.Text(), " ")
+	n1, _ := strconv.Atoi(s[0])
+	n2, _ := strconv.Atoi(s[1])
 
-	graph := make([][]int, n)
+	graph := make([][]int, n1)
 	for i := range graph {
 		scanner.Scan()
-		s := scanner.Text()
-		res := make([]int, len(s))
-		for j := range s {
-			k, _ := strconv.Atoi(string(s[j]))
-			res[j] = k
-		}
-		graph[i] = res
+		s2 := strings.Split(scanner.Text(), "")
+		graph[i] = transferSlice(s2, n2)
 	}
 
-	bfs(graph, 0, 0)
+	bfs(graph)
 
-	writer.WriteString(strconv.Itoa(graph[len(graph)-1][len(graph[0])-1]))
+	writer.WriteString(strconv.Itoa(graph[n1-1][n2-1]))
 }
