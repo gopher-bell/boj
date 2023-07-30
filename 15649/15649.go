71042f3627e9e4b93b8914d5c8806673d99e4651
Jong <gopher.bell@gmail.com>
2023-07-31 00:54:28 +0900

    boj: 15649

---
+++ 15649/15649.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var (
+	scanner = bufio.NewScanner(os.Stdin)
+	writer  = bufio.NewWriter(os.Stdout)
+	n, m    int
+	arr     [10]int
+	visited [10]bool
+)
+
+func getNumber() int {
+	ret := 0
+	scanner.Scan()
+	for _, v := range scanner.Bytes() {
+		ret *= 10
+		ret += int(v - '0')
+	}
+
+	return ret
+}
+
+func fn(k int) {
+	if k == m {
+		for i := 0; i < m; i++ {
+			writer.WriteString(strconv.Itoa(arr[i]) + " ")
+		}
+		writer.WriteString("\n")
+
+		return
+	}
+
+	for i := k; i <= n; i++ {
+		if !visited[i] {
+			visited[i] = true
+			arr[k] = i
+			fn(k + 1)
+			visited[i] = false
+		}
+	}
+}
+
+func main() {
+	defer writer.Flush()
+	scanner.Split(bufio.ScanWords)
+	n, m = getNumber(), getNumber()
+	fn(0)
+}
